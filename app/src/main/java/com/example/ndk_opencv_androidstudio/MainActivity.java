8771029ae8fe7ff43f04382cdf655b957a7b0df5
Jürgen Hahn <jurgenhahn@dhcp191238.uni-regensburg.de>
2015-10-19 15:28:03 +0200

    initial commit with working tooling

---
+++ app/src/main/java/com/example/ndk_opencv_androidstudio/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.ndk_opencv_androidstudio;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import org.opencv.android.CameraBridgeViewBase;
+public class MainActivity extends ActionBarActivity {
+
+    static {
+        System.loadLibrary("MyLib");
+        System.loadLibrary("opencv_java");
+    }
+    CameraBridgeViewBase camera;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.testTextView);
+        tv.setText(NativeClass.getStringFromNative());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/jni/com_example_ndk_opencv_androidstudio_NativeClass.cpp
@@ -0,0 +1,21 @@
+#include <com_example_ndk_opencv_androidstudio_NativeClass.h>
+#include "opencv2/opencv.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+JNIEXPORT jstring JNICALL Java_com_example_ndk_1opencv_1androidstudio_NativeClass_getStringFromNative(JNIEnv * env, jobject obj)
+{
+
+    Mat frame(17,7, CV_32FC2, Scalar(1,1));
+    char buf[128];
+    snprintf(buf, sizeof(buf), "%d", frame.cols);
+
+
+
+    return env->NewStringUTF(buf);
+}
