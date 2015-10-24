57fc319c2adf33c179fcb6f5bde342c570431294
JonathanBrem <jon.brem@t-online.de>
2015-10-24 16:52:44 +0200

    Changed to Google Face Detection (mobile vision).
    Did leave in the native support and OpenCV (although they are unused now). The FaceTrackerActivity can probably be deleted.

--- app/src/main/java/com/example/ndk_opencv_androidstudio/FaceTrackerActivity.java
+++ app/src/main/java/com/example/ndk_opencv_androidstudio/FaceTrackerActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.ndk_opencv_androidstudio;
 
 import android.Manifest;
@@ -7,18 +22,12 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.os.Environment;
+import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.SurfaceView;
 import android.view.View;
-import android.widget.TextView;
 
 import com.example.ndk_opencv_androidstudio.camera.CameraSourcePreview;
 import com.example.ndk_opencv_androidstudio.camera.GraphicOverlay;
@@ -30,17 +39,13 @@ import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
-import org.opencv.objdetect.CascadeClassifier;
-
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Activity for the face tracker app.  This app detects faces with the rear facing camera, and draws
+ * overlay graphics to indicate the position, size, and ID of each face.
+ */
+public final class FaceTrackerActivity extends AppCompatActivity {
     private static final String TAG = "FaceTracker";
 
     private CameraSource mCameraSource = null;
