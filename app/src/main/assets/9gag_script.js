2776553b27680cec7fcfe57ff0cda73aed997b33
JonathanBrem <jon.brem@t-online.de>
2015-11-08 00:54:39 +0100

    MemeView can now play animated images

--- app/src/main/assets/9gag_script.js
+++ app/src/main/assets/9gag_script.js
@@ -1,13 +1,69 @@
+var siteId;
+var vidStarted = false;
+
 jQuery(document).ready(function($) {
+
 	setTimeout(function() {
-        $("body").append("<div id='memeview_overlay'></div>");
-        setTimeout(function() {
-            $("#memeview_overlay").on("click", function(e) {
-                if(confirm("{url:\"" + window.location + "\", text:\"Sie können die Webseite in der App nur ansehen. Im Browser öffnen?\"}")) {
-                }
-                return false;
-            });
-        }, 100);
+        siteId = window.location.pathname.substring("/gag/".length);
+        if(siteId.indexOf("/") > 0) {
+            siteId = siteId.substring(0, siteId.indexOf("/"));
+        }
+
+        setupGeneralOverlay();
+
 
 	}, 500);
 });
+
+
+function setupGeneralOverlay() {
+    $("body").append("<div id='memeview_overlay'></div>");
+        setTimeout(function() {
+            $("#memeview_overlay").on("click", function(e) {
+                if(clickWasOnMeme(e)) {
+                    if(vidStarted) {
+                        $("#" + siteId + " .badge-post-media").trigger("click");
+                        vidStarted = false;
+                    } else {
+                        $("#" + siteId + " .badge-post-cover-image").trigger("click");
+                        vidStarted = true;
+                    }
+
+                } else {
+                    if(confirm("{url:\"" + window.location + "\", text:\"Sie können die Webseite in der App nur ansehen. Im Browser öffnen?\"}")) {};
+                }
+
+                return false;
+            });
+
+
+    }, 100);
+}
+
+function clickWasOnMeme(click) {
+    var possibleImages;
+
+    if(vidStarted) {
+      possibleImages = $("#" + siteId + " .badge-post-media");
+    } else {
+      possibleImages = $("#" + siteId + " .badge-post-cover-image");
+    }
+    var correctImg;
+    for(var i = 0; i < possibleImages.length; i++) {
+       if(possibleImages.eq(i).height() == 0) continue;
+       correctImg = possibleImages.eq(i);
+    }
+
+    if(correctImg == undefined || correctImg == null) return false;
+
+    var boundingRect = correctImg[0].getBoundingClientRect();
+
+    if(click.clientX  >= boundingRect.left && click.clientX <= boundingRect.right &&
+        click.clientY >= boundingRect.top && click.clientY <= boundingRect.bottom) {
+
+
+        return true;
+    }
+
+    return false;
+}
